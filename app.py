3b6d1dc7e91e3b886cc8c6ed3a0a506a3aabffa0
Said <saidbachar.uca.ma@gmail.com>
2021-05-26 17:48:37 +0100

    *> Add new items from GUI to db

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,12 +14,23 @@ class Todo(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    #Show all todos    
+    todo_list = Todo.query.all()
+    print(todo_list)
+    return render_template('base.html', todo_list = todo_list) #passing var to the html
 
-@app.route('/about')
-def about():
-    return "About"
+@app.route('/add', methods=['POST'])
+def add():
+   #Add new item
+   title =  request.form.get('title') #get title for the input
+   new_todo = Todo(title = title, complete = False)
+   db.session.add(new_todo)
+   db.session.commit()
+   return redirect(url_for("index")) # reload tye index page
+
 
 if __name__ == '__main__':
     db.create_all()
+
+   
     app.run(debug=True)
