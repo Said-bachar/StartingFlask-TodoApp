dab6061ef295293a2abda625ba9df6b2e0683c59
Said <saidbachar.uca.ma@gmail.com>
2021-05-26 18:57:14 +0100

    *> Adding Semantic Ui design

--- app.py
+++ app.py
@@ -19,14 +19,31 @@ def index():
     print(todo_list)
     return render_template('base.html', todo_list = todo_list) #passing var to the html
 
-@app.route('/add', methods=['POST'])
+@app.route("/add", methods=["POST"])
 def add():
    #Add new item
-   title =  request.form.get('title') #get title for the input
+   title =  request.form.get("title") #get title for the input
    new_todo = Todo(title = title, complete = False)
    db.session.add(new_todo)
    db.session.commit()
    return redirect(url_for("index")) # reload tye index page
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+   #Update item
+   todoToUpdate = Todo.query.filter_by(id = todo_id).first()
+   todoToUpdate.complete = not todoToUpdate.complete
+   db.session.commit()
+   return redirect(url_for("index")) # reload tye index page
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+   #Delete item
+   todoToUpdate = Todo.query.filter_by(id = todo_id).first()
+   db.session.delete(todoToUpdate)
+   db.session.commit()
+   return redirect(url_for("index")) # reload tye index page
+
 
 
 if __name__ == '__main__':
