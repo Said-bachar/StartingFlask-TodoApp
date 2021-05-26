d5403487c8098b53f30a61486844d08608fc03ec
Said <saidbachar.uca.ma@gmail.com>
2021-05-26 17:09:29 +0100

    *> Add base interface & install flask sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'  #path to db
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
